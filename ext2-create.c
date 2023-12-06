784f992067695967dfb99b2cf484e2e14bbb47b6
Krish Patel <krishhnpatel@gmail.com>
2023-12-06 02:11:05 -0800

    76

--- ext2-create.c
+++ ext2-create.c
@@ -508,13 +508,22 @@ void write_root_dir_block(int fd)
 	dir_entry_write(lost_and_found_entry,fd);
 	bytes_remaining -= lost_and_found_entry.rec_len;
 
+	struct ext2_dir_entry hello_world_entry = {0};
+    dir_entry_set(hello_world_entry, HELLO_WORLD_INO, "hello-world");
+    dir_entry_write(hello_world_entry, fd);
+    bytes_remaining -= hello_world_entry.rec_len;
+
+
+    struct ext2_dir_entry hello_entry = {0};
+    dir_entry_set(hello_entry, HELLO_INO, "hello");
+    dir_entry_write(hello_entry, fd);
+    bytes_remaining -= hello_entry.rec_len;
+
 	struct ext2_dir_entry fill_entry = {0};
     fill_entry.rec_len = bytes_remaining;
     dir_entry_write(fill_entry, fd);
 
-	// struct ext2_dir_entry hello_entry = {0};
-	// dir_entry_set(hello_entry, HELLO_INO, "hello");
-	// dir_entry_write(hello_entry, fd);
+
 
 }
 
