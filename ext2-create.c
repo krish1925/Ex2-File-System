1377fd5ecd85015d4b7291309940c2c6549eb9dd
Krish Patel <krishhnpatel@gmail.com>
2023-12-07 09:20:58 -0800

    dfnlakdsflkasdnlfa

--- ext2-create.c
+++ ext2-create.c
@@ -186,7 +186,7 @@ u32 get_current_time() {
 	}
 	return t;
 }
-/*
+
 void write_superblock(int fd) {
 	off_t off = lseek(fd, BLOCK_OFFSET(1), SEEK_SET);
 	if (off == -1) {
@@ -249,73 +249,6 @@ void write_superblock(int fd) {
 		errno_exit("write");
 	}
 }
-*/
-
-void write_superblock(int fd) {
-	off_t off = lseek(fd, BLOCK_OFFSET(1), SEEK_SET);
-	if (off == -1) {
-		errno_exit("lseek");
-	}
-
-	u32 current_time = get_current_time();
-
-	struct ext2_superblock superblock = {0};
-
-	// TODO It's all yours
-	// TODO finish the superblock number setting
-	superblock.s_inodes_count = NUM_INODES; //This value must be lower or equal to (s_inodes_per_group * number of block groups)
-	superblock.s_blocks_count = NUM_BLOCKS;
-	superblock.s_r_blocks_count = 0; 
-	superblock.s_free_blocks_count = NUM_FREE_BLOCKS;
-	superblock.s_free_inodes_count = NUM_FREE_INODES;
-	superblock.s_first_data_block = 1; //Note that this value is always 0 for file systems with a block size larger than 1KB, and always 1 for file systems with a block size of 1KB. The superblock is always starting at the 1024th byte of the disk, which normally happens to be the first byte of the 3rd sector.  /* First Data Block */
-	//SUPERBLOCK BLOCKNO?
-	superblock.s_log_block_size = 0;					/* 1024 */
-	superblock.s_log_frag_size = 0;						/* 1024 */
-	superblock.s_blocks_per_group = (1024 << superblock.s_log_block_size) * 8; //1024 bytes * 8 bits
-	superblock.s_frags_per_group =  (1024 << superblock.s_log_frag_size) * 8; //1024 bytes * 8 bits
-	superblock.s_inodes_per_group = (1024 << superblock.s_log_block_size) / (BLOCK_SIZE/NUM_INODES); //1024 / 8 inodes/block = 128
-	superblock.s_mtime = 0;				/* Mount time */
-	superblock.s_wtime = current_time;	/* Write time */
-	superblock.s_mnt_count         = 0; /* Number of times mounted so far */
-	superblock.s_max_mnt_count     = -1; /* Make this unlimited */
-	superblock.s_magic = EXT2_SUPER_MAGIC; /* ext2 Signature */
-	superblock.s_state             = 1; /* File system is clean */ //1: unmounted cleanly, 2: errors
-	superblock.s_errors            = 1; /* Ignore the error (continue on) */
-	superblock.s_minor_rev_level   = 0; /* Leave this as 0 */
-	superblock.s_lastcheck = current_time; /* Last check time */
-	superblock.s_checkinterval     = 1; /* Force checks by making them every 1 second */
-	superblock.s_creator_os        = 0; /* Linux */ //EXT2_OS_LINUX
-	superblock.s_rev_level         = 0; /* Leave this as 0 */
-	superblock.s_def_resuid        = 0; /* root */
-	superblock.s_def_resgid        = 0; /* root */
-
-	/* You can leave everything below this line the same, delete this
-	   comment when you're done the lab */
-	superblock.s_uuid[0] = 0x5A;
-	superblock.s_uuid[1] = 0x1E;
-	superblock.s_uuid[2] = 0xAB;
-	superblock.s_uuid[3] = 0x1E;
-	superblock.s_uuid[4] = 0x13;
-	superblock.s_uuid[5] = 0x37;
-	superblock.s_uuid[6] = 0x13;
-	superblock.s_uuid[7] = 0x37;
-	superblock.s_uuid[8] = 0x13;
-	superblock.s_uuid[9] = 0x37;
-	superblock.s_uuid[10] = 0xC0;
-	superblock.s_uuid[11] = 0xFF;
-	superblock.s_uuid[12] = 0xEE;
-	superblock.s_uuid[13] = 0xC0;
-	superblock.s_uuid[14] = 0xFF;
-	superblock.s_uuid[15] = 0xEE;
-
-	memcpy(&superblock.s_volume_name, "cs111-base", 10);
-
-	ssize_t size = sizeof(superblock);
-	if (write(fd, &superblock, size) != size) {
-		errno_exit("write");
-	}
-}
 
 void write_block_group_descriptor_table(int fd) {
 	off_t off = lseek(fd, BLOCK_OFFSET(BLOCK_GROUP_DESCRIPTOR_BLOCKNO), SEEK_SET);
@@ -339,7 +272,7 @@ void write_block_group_descriptor_table(int fd) {
 		errno_exit("write");
 	}
 }
-
+/*
 void write_block_bitmap(int fd)
 {
 	off_t off = lseek(fd, BLOCK_OFFSET(BLOCK_BITMAP_BLOCKNO), SEEK_SET);
@@ -415,6 +348,93 @@ void write_inode_bitmap(int fd)
 		errno_exit("write");
 	}
 }
+*/
+
+void write_block_bitmap(int fd)
+{
+	off_t off = lseek(fd, BLOCK_OFFSET(BLOCK_BITMAP_BLOCKNO), SEEK_SET);
+	if (off == -1)
+	{
+		errno_exit("lseek");
+	}
+
+	// TODO It's all yours
+	u8 map_value[BLOCK_SIZE] = {0};
+	int count = LAST_BLOCK;
+	int i = 0;
+	while(count > 0) {
+		if(count - 8 >= 0) { 
+			map_value[i] = 0xFF;
+			i++;
+			count -= 8;
+		} else {
+			//count % 8 is how many 1's there are, starting from the right
+			int mask = 0xFF;
+			for(int j = 0; j < 8 - count; j++) {
+				mask = mask >> 1;
+			}
+			
+			map_value[i] = mask;
+			count = 0;
+		}
+	}
+	// map_value[0] = 0xFF;
+	// map_value[1] = 0xFF; //  Free blocks: 24-1023
+	// map_value[2] = 0x7f; //Right to left, LSB to MSB.. 01111111 (24 23 22 21 20...)
+	map_value[127] = 0x80;  // 10000000
+	for(int i = 128; i < BLOCK_SIZE; i++)  { //SHOULD 128 BE REPLACED W SOMETHING? WHAT DOES THIS MEAN
+		map_value[i] = 0xFF;
+	}
+
+
+	if (write(fd, map_value, BLOCK_SIZE) != BLOCK_SIZE)
+	{
+		errno_exit("write");
+	}
+
+	////
+
+}
+
+void write_inode_bitmap(int fd)
+{
+	off_t off = lseek(fd, BLOCK_OFFSET(INODE_BITMAP_BLOCKNO), SEEK_SET);
+	if (off == -1)
+	{
+		errno_exit("lseek");
+	}
+
+	// TODO It's all yours
+	u8 map_value[BLOCK_SIZE] = {0};
+
+	int count = LAST_INO;
+	int i = 0;
+	while(count > 0) {
+		if(count - 8 >= 0) { 
+			map_value[i] = 0xFF;
+			i++;
+			count -= 8;
+		} else {
+			//count % 8 is how many 1's there are, starting from the right
+			int mask = 0xFF;
+			for(int j = 0; j < 8 - count; j++) {
+				mask = mask >> 1;
+			}
+			
+			map_value[i] = mask;
+			count = 0;
+		}
+	}
+	for(int i = 16; i < BLOCK_SIZE; i++)  { //SHOULD 128 BE REPLACED W SOMETHING? WHAT DOES THIS MEAN
+		map_value[i] = 0xFF;
+	}
+
+	if (write(fd, map_value, BLOCK_SIZE) != BLOCK_SIZE)
+	{
+		errno_exit("write");
+	}
+}
+
 void write_inode(int fd, u32 index, struct ext2_inode *inode) {
 	off_t off = BLOCK_OFFSET(INODE_TABLE_BLOCKNO)
 	            + (index - 1) * sizeof(struct ext2_inode);
